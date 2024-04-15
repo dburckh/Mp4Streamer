3c6ba6a5bd1431b39e2da914551e8e6cbe602fac
Dustin <dburckh@gmail.com>
2024-04-15 12:28:34 -0600

    Port Media3 in

--- lib-muxer/build.gradle.kts
+++ lib-muxer/build.gradle.kts
@@ -11,8 +11,18 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-    }
 
+    }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+    sourceSets {
+        this.getByName("test"){
+            this.assets.srcDir("./assets")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,6 +46,10 @@ dependencies {
     compileOnly("org.checkerframework:checker-qual:3.13.0")
     compileOnly("com.google.errorprone:error_prone_annotations:2.18.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("org.robolectric:robolectric:4.11")
+    implementation("androidx.media3:media3-test-utils:1.3.0")
+    implementation("androidx.media3:media3-extractor:1.3.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
