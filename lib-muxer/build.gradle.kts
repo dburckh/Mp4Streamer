c926627406541a9f9b11ba414763adc0d8bd692d
Dustin <dburckh@gmail.com>
2024-06-01 15:04:37 -0600

    Jitpack.io build

--- lib-muxer/build.gradle.kts
+++ lib-muxer/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.library")
+    id("maven-publish")
 }
 
 android {
@@ -36,20 +37,40 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    //Install to local maven repo
+    //gradlew libraw:publishToMavenLocal
+    publishing {
+        singleVariant("release")
+    }
+}
+//Install to local maven repo
+//gradlew lib-muxer:publishToMavenLocal
+afterEvaluate {
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                // These are ignored by Jitpack.io
+                groupId = "com.homesoft.android"
+                artifactId = "muxer"
+                version = "0.9.0"
+                from(components["release"])
+            }
+        }
+    }
 }
 
 dependencies {
 
     api("androidx.media3:media3-common:1.3.0")
     implementation("androidx.media3:media3-container:1.3.0")
+    implementation("androidx.media3:media3-extractor:1.3.0")
     implementation("androidx.annotation:annotation:1.7.1")
     compileOnly("org.checkerframework:checker-qual:3.13.0")
     compileOnly("com.google.errorprone:error_prone_annotations:2.18.0")
+    testImplementation("androidx.media3:media3-test-utils:1.3.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("org.robolectric:robolectric:4.11")
-    implementation("androidx.media3:media3-test-utils:1.3.0")
-    implementation("androidx.media3:media3-extractor:1.3.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
